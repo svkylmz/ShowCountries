ba99da5f131020a857ea86f01f988c6847a0abd1
svkylmz <Ylmazsevket@gmail.com>
2021-09-10 12:13:28 +0300

    create country recycler view adapter

---
+++ app/src/main/java/com/svkylmz/showcountries/adapter/CountryAdapter.kt
@@ -0,0 +1,37 @@
+package com.svkylmz.showcountries.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.svkylmz.showcountries.R
+import com.svkylmz.showcountries.model.Country
+import kotlinx.android.synthetic.main.country_list_item.view.*
+
+class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+    class CountryViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.country_list_item, parent, false)
+        return CountryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.view.countryNameText.text = countryList[position].countryName
+        holder.view.capitalNameText.text = countryList[position].countryCapital
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+    fun updateCountryList(newCountryList: List<Country>) {
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()  // refresh the adapter
+    }
+}
