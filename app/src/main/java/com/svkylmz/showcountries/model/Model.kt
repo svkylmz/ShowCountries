48f94bd8f48cf9a35b6c5867127b4dc98931b883
svkylmz <Ylmazsevket@gmail.com>
2021-09-11 13:38:57 +0300

    set model with room library to save downloaded data from internet into database

--- app/src/main/java/com/svkylmz/showcountries/model/Model.kt
+++ app/src/main/java/com/svkylmz/showcountries/model/Model.kt
@@ -1,23 +1,36 @@
 package com.svkylmz.showcountries.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Country (
-    @SerializedName("name")
+    @ColumnInfo(name = "name")       // Room
+    @SerializedName("name")    // Retrofit
     val countryName: String?,
 
+    @ColumnInfo(name = "region")
     @SerializedName("region")
     val countryRegion: String?,
 
+    @ColumnInfo(name = "capital")
     @SerializedName("capital")
     val countryCapital: String?,
 
+    @ColumnInfo(name = "currency")
     @SerializedName("currency")
     val countryCurrency: String?,
 
+    @ColumnInfo(name = "language")
     @SerializedName("language")
     val countryLanguage: String?,
 
+    @ColumnInfo(name = "flag")
     @SerializedName("flag")
     val countryFlagImageUrl: String?
-    )
+    ) {
+    @PrimaryKey(autoGenerate = true)
+    val uuid: Int = 0
+}
