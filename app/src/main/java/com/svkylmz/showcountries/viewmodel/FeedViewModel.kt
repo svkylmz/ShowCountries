bd859105f64f6b40cc6c9574ede66e05b1dea350
svkylmz <Ylmazsevket@gmail.com>
2021-09-11 15:50:57 +0300

    add operations which store downloaded data from internet with retrofit room database by using coroutine skills

--- app/src/main/java/com/svkylmz/showcountries/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/svkylmz/showcountries/viewmodel/FeedViewModel.kt
@@ -1,16 +1,17 @@
 package com.svkylmz.showcountries.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.svkylmz.showcountries.model.Country
 import com.svkylmz.showcountries.service.CountryAPIService
-import io.reactivex.Scheduler
+import com.svkylmz.showcountries.service.CountryDatabase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class FeedViewModel: ViewModel() {
+class FeedViewModel(application: Application): BaseViewModel(application) {
     private val countryApiService = CountryAPIService()
     private val disposable = CompositeDisposable()
 
@@ -31,9 +32,7 @@ class FeedViewModel: ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
                     override fun onSuccess(t: List<Country>) {
-                        countries.value = t
-                        countryError.value = false
-                        countryLoading.value = false
+                        storeInSqlite(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -41,8 +40,28 @@ class FeedViewModel: ViewModel() {
                         countryError.value = true
                         e.printStackTrace()
                     }
-
                 })
         )
+    }
+
+    private fun showCountries(countryList: List<Country>) {
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+
+    private fun storeInSqlite(list: List<Country>) {
+        //background actions without blocking the UI
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries() //first deleting operation
+            val listLong = dao.insertAll(*list.toTypedArray())  // list -> individual elements
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = listLong[i].toInt()
+                i += 1
+            }
+            showCountries(list)
+        }
     }
 }
